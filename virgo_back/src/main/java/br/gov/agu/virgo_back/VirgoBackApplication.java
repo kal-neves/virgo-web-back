1ef937c2b65c75b48ad0f0111773036c2bd3f0fe
Kallel <kallelsneves.py@gmail.com>
2026-08-28 12:13:16 -0300

    chore: root

---
+++ virgo_back/src/main/java/br/gov/agu/virgo_back/VirgoBackApplication.java
@@ -0,0 +1,13 @@
+package br.gov.agu.virgo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirgoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirgoBackApplication.class, args);
+	}
+
+}
---
+++ virgo_back/src/test/java/br/gov/agu/virgo_back/VirgoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.agu.virgo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirgoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
